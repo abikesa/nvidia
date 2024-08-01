6bd3bd35249010a3791dcdf6cd40e4b734dcc271
abikesa <abikesa.sh@gmail.com>
2024-08-01 19:30:54 -0400

    jesus-loves-me.mp3 added to scripts/

--- kitabo/ensi/scripts/jb_clean.sh
+++ kitabo/ensi/scripts/jb_clean.sh
@@ -4,4 +4,5 @@ cp pdfs/locke.pdf _build/html/pdfs/locke.pdf
 cp pdfs/bach.pdf _build/html/pdfs/bach.pdf
 cp pdfs/handel.pdf _build/html/pdfs/handel.pdf
 cp pdfs/mozart.pdf _build/html/pdfs/mozart.pdf
+cp media/jesus-loves-me.mp3 _build/html/media/jesus-loves-me.mp3
 cp -r act1/app/* _build/html/act1/app
