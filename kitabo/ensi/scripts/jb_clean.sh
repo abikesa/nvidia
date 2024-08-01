ef619ec9c261595462576512b4ac2ce76dca9105
abikesa <abikesa.sh@gmail.com>
2024-08-01 16:39:18 -0400

    jensen has a service just for me

---
+++ kitabo/ensi/scripts/jb_clean.sh
@@ -0,0 +1,7 @@
+# transfer after every jb clean .
+mkdir -p _build/html/pdfs  
+cp pdfs/locke.pdf _build/html/pdfs/locke.pdf
+cp pdfs/bach.pdf _build/html/pdfs/bach.pdf
+cp pdfs/handel.pdf _build/html/pdfs/handel.pdf
+cp pdfs/mozart.pdf _build/html/pdfs/mozart.pdf
+cp -r act1/app/* _build/html/act1/app
